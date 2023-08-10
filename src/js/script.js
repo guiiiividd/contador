9c5272605f30bbc2cc1a8e5dc599a4ef27b46b2d
guiiiividd <guiiiividd@gmail.com>
2023-08-10 18:01:49 -0300

    Deixando o Contador Funcional

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-console.log("Olá mundo!");
+const counter = document.getElementById('counter-value')
+const resetButton = document.getElementById('reset-button')
+const incrementButton = document.getElementById('increment-button')
+const decrementButton = document.getElementById('decrement-button')
+
+counter.innerText = localStorage.getItem("value");
+
+resetButton.addEventListener('click', ()=>{
+    localStorage.setItem("value", 0);
+    counter.innerText = localStorage.getItem("value");
+})
+
+incrementButton.addEventListener('click', ()=>{
+    localStorage.setItem("value", parseInt(localStorage.getItem("value")) + 1);
+    counter.innerText = localStorage.getItem("value");
+})
+
+decrementButton.addEventListener('click', ()=>{
+    localStorage.setItem("value", parseInt(localStorage.getItem("value")) - 1);
+    counter.innerText = localStorage.getItem("value");
+})
